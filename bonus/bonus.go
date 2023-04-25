21ad08474478193056aedee86ad8e805e1255a35
Guilherme Fernandes <126535551+guifzy@users.noreply.github.com>
2023-04-25 10:31:18 -0300

    Update bonus.go

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,5 +1,7 @@
 package bonus
 
+import "sort"
+
 //Joãozinho ganhou um kit de construção de torres. O kit é composto por várias barras de madeira, e seus comprimentos são
 //conhecidos. As barras podem ser empilhadas umas sobre as outras, desde que seus comprimentos sejam iguais.
 //
@@ -7,6 +9,24 @@ package bonus
 //barras da melhor maneira possível, determinando a altura da torre mais alta e quantas torres podem ser construídas.
 
 func CalculateTowers(barLengths []int) (int, int) {
-	// Seu código aqui
-	return 0, 0
+	maiorTorre := 1
+	midTorre := 1
+	num_torres := len(barLengths)
+	sort.Ints(barLengths)
+	elemento := 0
+	for elemento < (len(barLengths) - 1) {
+		if barLengths[elemento] == barLengths[elemento+1] {
+			midTorre += 1
+			if midTorre > maiorTorre && midTorre > 1 {
+				midTorre = midTorre
+			}
+			num_torres -= 1
+
+		} else {
+			midTorre = 1
+		}
+		elemento++
+	}
+
+	return maiorTorre, num_torres
 }
